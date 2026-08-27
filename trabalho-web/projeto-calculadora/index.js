d8e85f3cce375f293cedaa4b694f30e17816c4eb
JKing <78988532+JoaoVictorKarline@users.noreply.github.com>
2026-08-27 06:59:05 -0300

    Initialize index.js for calculator project

---
+++ trabalho-web/projeto-calculadora/index.js
@@ -0,0 +1,8 @@
+let numero_atual = "";
+let numero_anterior = "";
+
+let botao_numeros = document.getElementByClassname("num");
+let botao_operadores = document.getElementByClassname("operadorbotao");
+let botao_igual = document.getElementById("igual")
+
+
